ca9f746e02fd24bb721b66267464a7f4a2c42e6b
ks5448 <ks5448@nyu.edu>
2023-10-10 18:03:35 -0400

    6

--- smtpClient.py
+++ smtpClient.py
@@ -1,7 +1,7 @@
 from socket import *
 
 
-def smtp_client(port=25, mailserver='smtp.freesmtpservers.com'):#(port=1025, mailserver='127.0.0.1'):
+def smtp_client(port=1025, mailserver='127.0.0.1'):
     msg = "\r\n My message"
     endmsg = "\r\n.\r\n"
 
@@ -64,10 +64,9 @@ def smtp_client(port=25, mailserver='smtp.freesmtpservers.com'):#(port=1025, mai
     quitCommand = 'QUIT\r\n'
     clientSocket.send(quitCommand.encode())
     recv6 = clientSocket.recv(1024).decode()
-    #clientSocket.close()
+    clientSocket.close()
     # Fill in end
 
 
 if __name__ == '__main__':
-    smtp_client(25, 'freesmtpservers.com')
-    #smtp_client(1025, '127.0.0.1')
+    smtp_client(1025, '127.0.0.1')
