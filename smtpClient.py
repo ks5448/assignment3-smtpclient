b61da81e9a320f3af1901a5be753bcc436414c8e
ks5448 <ks5448@nyu.edu>
2023-10-10 18:16:30 -0400

    8

--- smtpClient.py
+++ smtpClient.py
@@ -20,8 +20,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     #    print('220 reply not received from server.')
 
     # Send HELO command and print server response.
-    heloCommand = 'HELO Alice\r\n'
-    clientSocket.send(heloCommand.encode())
+    #heloCommand = 'HELO Alice\r\n'
+    clientSocket.send('HELO Alice\r\n'.encode())
     recv1 = clientSocket.recv(1024).decode()
     #print(recv1) 
     #if recv1[:3] != '250':
